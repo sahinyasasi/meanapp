7e7d412950f3300737dd6e1d485d69732e1c2650
sahinyasasi <sahinya.slc@gmail.com>
2020-12-14 14:25:21 +0530

    created schema

--- contactlist/models/contacts.js
+++ contactlist/models/contacts.js
@@ -1,2 +1,16 @@
 const mongoose = require("mongoose");
-const ContactSchema;
+const ContactSchemaa = mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+const Contact = (module.exports = mongoose.model("Contact", ContactSchemaa));
