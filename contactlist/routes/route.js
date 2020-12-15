09dcce227cc6ab9d9b9948d5b2d6b154640fbc7d
sahinyasasi <sahinya.slc@gmail.com>
2020-12-15 10:49:17 +0530

    used postman

--- contactlist/routes/route.js
+++ contactlist/routes/route.js
@@ -14,7 +14,7 @@ router.get("/contacts", (req, res, next) => {
 router.post("/contact", (req, res, next) => {
   let newContact = new Contact({
     first_name: req.body.first_name,
-    lasst_name: req.body.last_name,
+    last_name: req.body.last_name,
     phone: req.body.phone,
   });
   newContact.save((err, contact) => {
